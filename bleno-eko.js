4d407484956b7c644ba909b23da7475de811007c
Johannes Edelstam <johannes@edelst.am>
2014-10-23 22:58:43 +0200

    bleno

---
+++ bleno-eko.js
@@ -0,0 +1,81 @@
+var bleno = require('bleno');
+
+var util = require('util'),
+  os = require('os'),
+  exec = require('child_process').exec,
+  bleno = require('bleno'),
+  Descriptor = bleno.Descriptor,
+  Characteristic = bleno.Characteristic;
+
+var BatteryLevelCharacteristic = function() {
+  BatteryLevelCharacteristic.super_.call(this, {
+      uuid: '2A19',
+      properties: ['read'],
+      descriptors: [
+        new Descriptor({
+            uuid: '2901',
+            value: 'Battery level between 0 and 100 percent'
+        }),
+        new Descriptor({
+            uuid: '2904',
+            value: new Buffer([0x04, 0x01, 0x27, 0xAD, 0x01, 0x00, 0x00 ]) // maybe 12 0xC unsigned 8 bit
+        })
+      ]
+  });
+};
+
+util.inherits(BatteryLevelCharacteristic, Characteristic);
+
+BatteryLevelCharacteristic.prototype.onReadRequest = function(offset, callback) {
+
+  if (os.platform() === 'darwin') {
+    exec('pmset -g batt', function (error, stdout, stderr) {
+
+      var data = stdout.toString();
+      // data - 'Now drawing from \'Battery Power\'\n -InternalBattery-0\t95%; discharging; 4:11 remaining\n'
+      var percent = data.split('\t')[1].split(';')[0];
+      console.log(percent);
+      percent = parseInt(percent, 10);
+      callback(this.RESULT_SUCCESS, new Buffer([percent]));
+
+    });
+  } else {
+    // return hardcoded value
+    callback(this.RESULT_SUCCESS, new Buffer([98]));
+  }
+};
+
+var util = require('util'),
+  bleno = require('bleno'),
+  BlenoPrimaryService = bleno.PrimaryService;
+
+function BatteryService() {
+  BatteryService.super_.call(this, {
+      uuid: '180F',
+      characteristics: [
+          new BatteryLevelCharacteristic()
+      ]
+  });
+}
+
+util.inherits(BatteryService, BlenoPrimaryService);
+
+var primaryService = new BatteryService()
+
+bleno.on('stateChange', function(state) {
+    console.log('on -> stateChange: ' + state);
+
+    if (state === 'poweredOn') {
+        bleno.startAdvertising('Battery', [primaryService.uuid]);
+    } else {
+        bleno.stopAdvertising();
+    }
+});
+
+bleno.on('advertisingStart', function(error) {
+    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+    if (!error) {
+        bleno.setServices([primaryService]);
+    }
+});
